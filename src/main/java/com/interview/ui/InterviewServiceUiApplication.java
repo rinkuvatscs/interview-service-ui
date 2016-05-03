a8e3623cd05badb331452104ba95ec14510ee8bc
Rinku <rinkuvatscs@gmail.com>
2016-05-03 08:00:09 +0530

    InterviewService-ui

---
+++ src/main/java/com/interview/ui/InterviewServiceUiApplication.java
@@ -0,0 +1,12 @@
+package com.interview.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewServiceUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewServiceUiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/interview/ui/InterviewServiceUiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.interview.ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = InterviewServiceUiApplication.class)
+@WebAppConfiguration
+public class InterviewServiceUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
